8e36ef0c8ef5fd3c3e621e0816135def009a9aaf
bcarp <bcarp@local>
2026-01-27 23:43:09 -0800

    Add ErrorBoundary to debug blank site

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { GamificationProvider } from './context/GamificationContext'
+import ErrorBoundary from './components/ErrorBoundary'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GamificationProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </GamificationProvider>
+    <ErrorBoundary>
+      <GamificationProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </GamificationProvider>
+    </ErrorBoundary>
   </StrictMode>,
 )
